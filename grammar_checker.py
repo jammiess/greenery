0f2d15c615591b2af7440e3e1618dee7df9187cc
MegaIng1 <trampchamp@hotmail.de>
2019-07-25 00:41:28 +0200

    - small improvements

--- grammar_checker.py
+++ grammar_checker.py
@@ -1,15 +1,16 @@
+import time
+
 from lark import Lark
 
 from pattern_parser import parse_pattern, compare_patterns
 
-grammar = Lark(open("grammar.lark"),parser='lalr',start='file_input')
-regexps = [(term, parse_pattern(term.pattern.to_regexp())) for term in grammar._build_lexer().terminals]
-# print("parsed")
-# for t, p in regexps:
-#     print(t)
-#     print(p)
-#     print(p.to_fsm())
-#     print()
+start = time.time()
+grammar = Lark(open("grammar.lark"), parser='lalr', start='file_input')
+regexps = [(term, parse_pattern(term.pattern.to_regexp())) for term in grammar._build_lexer().terminals if
+           term.pattern.type == 're']
+print(len(regexps))
 pat_term_map = {p: t for t, p in regexps}
 for a, b in compare_patterns(*pat_term_map.keys()):
     print(f"Collision between {pat_term_map[a]} and {pat_term_map[b]}")
+end = time.time()
+print(f"Total time: {end - start}")
